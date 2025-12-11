97f1e209d80197fa23656e69ebb3196bfd51dea5
Zuha Aqib <117381907+z-aqib@users.noreply.github.com>
2025-12-11 22:20:00 +0500

    added all the remaining labs and corrected the folder naming convention

---
+++ labs/lab01_intro-to-sql/testdb.sql
@@ -0,0 +1,11 @@
+CREATE TABLE PLAYER (
+playerID int,
+FirstName varchar(255),
+LastName varchar(255),
+Sport varchar(255)
+);
+
+INSERT INTO PLAYER (playerID, FirstName, LastName, Sport) VALUES (1, 'Zuha', 'Aqib', 'Basketball')
+INSERT INTO PLAYER (playerID, FirstName, LastName, Sport) VALUES (2, 'Zehra', 'Ahmed', 'Badminton')
+
+SELECT * FROM PLAYER
